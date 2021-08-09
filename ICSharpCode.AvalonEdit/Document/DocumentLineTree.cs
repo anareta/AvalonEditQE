fb2cfbf337c874d34e5e1cb8b855a4d631ff46f2
Az <vbvbnbnvn@gmail.com>
2021-08-10 03:15:03 +0900

    邪魔なデバッグ出力を削除

--- ICSharpCode.AvalonEdit/Document/DocumentLineTree.cs
+++ ICSharpCode.AvalonEdit/Document/DocumentLineTree.cs
@@ -103,7 +103,9 @@ namespace ICSharpCode.AvalonEdit.Document
 			Debug.Assert(nodes.Length > 0);
 			// now build the corresponding balanced tree
 			int height = GetTreeHeight(nodes.Length);
+			/*
 			Debug.WriteLine("DocumentLineTree will have height: " + height);
+			*/
 			root = BuildTree(nodes, 0, nodes.Length, height);
 			root.color = BLACK;
 			#if DEBUG
